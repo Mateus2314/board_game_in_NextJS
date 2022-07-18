5753e77374bcca269373059ec6e4f7df956fd0ce
Mateus2314 <mateusfuntecezar10@hotmail.com>
2022-07-18 15:33:01 -0300

    Init project

---
+++ components/Linha.jsx
@@ -0,0 +1,22 @@
+import Subdivisao from "./Subdivisao";
+import styles from "../styles/Linha.module.css"
+
+
+export default function Linha(props){
+  return (
+    <div className={ styles.linha } >
+     
+     <Subdivisao preta={ props.preta } ></Subdivisao>
+     <Subdivisao preta={ !props.preta } ></Subdivisao>
+     <Subdivisao preta={ props.preta } ></Subdivisao>
+     <Subdivisao preta={ !props.preta } ></Subdivisao>
+     <Subdivisao preta={ props.preta } ></Subdivisao>
+     <Subdivisao preta={ !props.preta } ></Subdivisao>
+     <Subdivisao preta={ props.preta } ></Subdivisao>
+     <Subdivisao preta={ !props.preta } ></Subdivisao>
+
+      
+
+    </div>
+  )
+}
